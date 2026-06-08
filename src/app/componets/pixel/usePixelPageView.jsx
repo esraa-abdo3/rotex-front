0f2406372d763ab99a053e9e4619a25f8f8ca308
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2026-06-09 00:16:18 +0300

    test test tetr test

--- src/app/componets/pixel/usePixelPageView.jsx
+++ src/app/componets/pixel/usePixelPageView.jsx
@@ -3,13 +3,18 @@ import { useEffect } from "react";
 
 export function usePixelPageView() {
   useEffect(() => {
+    if (typeof window === "undefined") return;
     if (!window.fbq) return;
 
     const fired = sessionStorage.getItem("pixel_pv_fired");
 
-    if (!fired) {
-      fbq("track", "PageView");
-      sessionStorage.setItem("pixel_pv_fired", "1");
+      if (fired) {
+          console.log("exist")
+          
+        
     }
+
+    window.fbq("track", "PageView");
+    sessionStorage.setItem("pixel_pv_fired", "1");
   }, []);
 }
