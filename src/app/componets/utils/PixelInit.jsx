18f3b51d2cc70534ea8683aa64758542ee07427d
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2026-06-08 19:21:32 +0300

    test after

--- src/app/componets/utils/PixelInit.jsx
+++ src/app/componets/utils/PixelInit.jsx
@@ -1,3 +1,40 @@
+// "use client";
+
+// import Script from "next/script";
+
+// const PIXEL_ID = "2496490754109919";
+
+// export default function PixelInit() {
+//   return (
+//     <>
+//       <Script
+//         id="meta-pixel-inline"
+//         strategy="afterInteractive"
+//         dangerouslySetInnerHTML={{
+//           __html: `
+//             !function(f,b,e,v,n,t,s){
+//               if(f.fbq)return;
+//               n=f.fbq=function(){n.callMethod?
+//               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+//               if(!f._fbq)f._fbq=n;
+//               n.push=n;n.loaded=!0;n.version='2.0';
+//               n.queue=[];
+//             }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+//             fbq('init', '${PIXEL_ID}', {}, { autoConfig: false });
+//             window._pixelInitialized = true;
+//             window.dispatchEvent(new Event('fbq:ready'));
+//           `,
+//         }}
+//       />
+
+//       <Script
+//         id="meta-pixel-sdk"
+//         strategy="afterInteractive"
+//         src="https://connect.facebook.net/en_US/fbevents.js"
+//       />
+//     </>
+//   );
+// }
 "use client";
 
 import Script from "next/script";
@@ -7,8 +44,9 @@ const PIXEL_ID = "2496490754109919";
 export default function PixelInit() {
   return (
     <>
+      {/* الـ SDK والـ init في script واحدة بالترتيب الصح */}
       <Script
-        id="meta-pixel-inline"
+        id="meta-pixel-full"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -19,18 +57,15 @@ export default function PixelInit() {
               if(!f._fbq)f._fbq=n;
               n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];
+              t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
             }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '${PIXEL_ID}', {}, { autoConfig: false });
             window._pixelInitialized = true;
             window.dispatchEvent(new Event('fbq:ready'));
           `,
         }}
-      />
-
-      <Script
-        id="meta-pixel-sdk"
-        strategy="afterInteractive"
-        src="https://connect.facebook.net/en_US/fbevents.js"
       />
     </>
   );
